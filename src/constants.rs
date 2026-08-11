5a66ccffb368b92998510d9598e02b3f96e07bbf
stanley-i2p <268402168+stanley-i2p@users.noreply.github.com>
2026-08-11 13:21:44 -0400

    version 1.0.0

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub const APP_NAME: &str = "IcedComm-I2P";
-pub const APP_VERSION: &str = "1.0.0-beta.2";
+pub const APP_VERSION: &str = "1.0.0";
 pub const APP_FONT_FAMILY: &str = "Inter";
 pub const APP_ICON_FONT_FAMILY: &str = "Material Symbols Rounded";
 
